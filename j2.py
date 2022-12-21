95013fc5f598bc146ef5f5b8500df91a3de1f44e
JafarbekH <zokirovjafar2004@gmail.com>
2022-12-21 19:46:12 +0500

    solu2

---
+++ j2.py
@@ -0,0 +1,11 @@
+# Radiuslari r1, r2, r3 bo’lgan 3 ta doira radiuslari berilgan. Doiralarni yuzlarini hisoblang.
+r1=int(input('r1='))
+r2=int(input('r2='))
+r3=int(input('r3='))
+from math import pi
+S1=(pi)*r1**2
+S2=(pi)*r2**2
+S3=(pi)*r3**2
+print(S1)
+print(S2)
+print(S3)
