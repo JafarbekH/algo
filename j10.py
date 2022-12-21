ca7194df333fa23d5dac032b0596102095a9f443
JafarbekH <zokirovjafar2004@gmail.com>
2022-12-21 21:58:37 +0500

    solu10

---
+++ j10.py
@@ -0,0 +1,11 @@
+# jo`mrakdan 1 s da 1 milli litr suv tomsa x yilda necha litr suv tomadi.
+''' birinchi o'rinda 1 yilda necha litr suv tomishini tooib olamiz
+1 yilda 365 kun bo'lsa 365*24 soat vaqt bor va 365*24*3600 sekunt bor demak
+1 yilda 365*24*3600 millilitr suv oqadi uni litrga otkazsak 365*24*3.6 litr sub oqadidemak x yilda 
+x*365*24*3.6 litr suv oqadi '''
+
+x=int(input("x="))
+
+v=x*365*24*3.6
+
+print(v)
