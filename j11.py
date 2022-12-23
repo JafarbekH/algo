5a87338e4c5c02f1e38582e5ec1906764e14cd34
JafarbekH <zokirovjafar2004@gmail.com>
2022-12-23 19:48:52 +0500

    solu11

---
+++ j11.py
@@ -0,0 +1,7 @@
+def progressiya(n):
+     ''' 1 dan n gacha bo'lgan sonlarning yig'indsini hisoblash uchun arifmetik progressiya fo'r-
+     mulasidan foydalanamiz. bunda hadlar soni n ta d esa 1 ga teng bo'ladi'''
+
+     answer=((1+n)*n/2)
+     return answer
+print(progressiya(9))
