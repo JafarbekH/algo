cf2925ad6197c9868298d17196486087b15965b8
JafarbekH <zokirovjafar2004@gmail.com>
2022-12-24 14:55:39 +0500

    uyga17

--- j17.py
+++ j17.py
@@ -1,5 +1,5 @@
 def funksiya(x,y):
     from math import tan, cos, log, pi
-    f1=((2*tan(x+pi/6))/((1/3)+pow(cos(y+pow(x,2)),2)))+log(2,pow(x,2)+2) 
+    f1=((2*tan(x+pi/6))/((1/3)+pow(cos(y+pow(x,2)),2)))+log(pow(x,2)+2,2) 
     return round(f1,2) 
 print(funksiya(1.7,5.18))
