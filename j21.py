1c70506d4e5f437e617accaae9c9b67e9f12756f
JafarbekH <zokirovjafar2004@gmail.com>
2022-12-26 14:44:25 +0500

    solu21

--- j21.py
+++ j21.py
@@ -1,5 +1,4 @@
-def main(a,b,c,x):
-    from math import cos, sqrt
-    w1=0.75+(8.2*pow(x,2)+sqrt(abs(pow(x,3)+3*x)+cos(x-2)))/((a/4)+(b/3)+(c/2)+1)
-    return round(w1,2)
-print(main(1,1,1,0.33))
+def main(a,b):
+    T=pow(a,1/5)+pow(((a+b)*b)/(2*b+a*b),1/4)*(pow(a,2)+pow(b,2)+2)
+    return round(T,2)
+print(main(0.36,1.02))
