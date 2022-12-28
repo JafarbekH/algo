14f6963356e34e04f46e50eb4ff7315abf5c8a0c
JafarbekH <zokirovjafar2004@gmail.com>
2022-12-28 08:19:38 +0500

    solu32

---
+++ j32.py
@@ -0,0 +1,16 @@
+x = float(input())
+y = float(input())
+z = float(input())
+
+if x>y>z :
+    print(x, z)
+if y>x>z :
+    print(y, z)
+if z>x>y :
+    print(z, y) 
+if x>z>y :
+    print(x, y)
+if z>y>x :
+    print(z, x)
+if y>z>x :
+    print(y, x)
