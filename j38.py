3b8a9f8216a266fe2d0b271a02bd82c2f50e93c5
JafarbekH <zokirovjafar2004@gmail.com>
2022-12-30 08:29:01 +0500

    solu38

---
+++ j38.py
@@ -0,0 +1,20 @@
+a = int(input())
+b = int(input())
+c = int(input())
+
+if 0<a<4: 
+    print(a)
+if 0<b<4: 
+    print(b)
+if 0<c<4: 
+    print(c)
+if 0<a<4 and 0<b<4: 
+    print(a, b)
+if 0<c<4 and 0<b<4: 
+    print(c, b)
+if 0<a<4 and 0<c<4: 
+    print(a, c)
+if 0<a<4 and 0<b<4 and 0<c<4:  
+    print(a, b, c)
+
+    
