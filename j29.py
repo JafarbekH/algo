60617391d62f89db119cfc23a8267bde07b599fa
JafarbekH <zokirovjafar2004@gmail.com>
2022-12-30 08:59:45 +0500

    solu29

---
+++ j29.py
@@ -0,0 +1,5 @@
+def main(a,x,y):
+    from math import e,cos,sin
+    TT=pow((pow(y,2)+pow(e,x)+pow(pow(((e,x)+a/(pow(x,2)+2)),1/2))+pow(cos(x),2)/sin(pow(x,2))),1/2)+pow(cos(x),3)
+    return round(TT,2)
+print(main(0,3.97,1.49))
