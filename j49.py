707e1768f6891892690439c3d259d3917a26d5ed
JafarbekH <zokirovjafar2004@gmail.com>
2022-12-30 14:58:28 +0500

    solu49

---
+++ j49.py
@@ -0,0 +1,12 @@
+a = float(input('a='))
+
+#Endi funksiya sini o'zgarishini hisoblab chiqamiz har bir siniq chiziq uchun.
+
+if a<-1 :
+    print(-a-1)
+if a>-2 and a<1 :
+    print(round(a+1,2))
+if a>0 and a<=1 :
+    print(round(-a+1,2))
+if a>1:
+    print(round(a-1,2))
