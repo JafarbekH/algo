2ce0490950efb3665808f0e0068abd94c4b38ade
JafarbekH <zokirovjafar2004@gmail.com>
2023-01-03 17:33:42 +0500

    solu50

---
+++ j50.py
@@ -0,0 +1,7 @@
+x = float(input('x ga qiymat kiriting x='))
+y = float(input('y ga qiymat kiriting y='))
+
+if -2/3<=x<=2/3 and 0<=y<=2 or -1<=x<=1 and -1<=y<=0 :
+    print('yes')
+else:
+    print('no')
